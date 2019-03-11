0376c15d943b263554132b7f073a97d9e0a94735
Aamir <aamir.zaidi2000@gmail.com>
2019-03-11 09:21:07 -0700

    Done

--- Main.js
+++ Main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
         if (a[0].value == "" || isNum == true || a[0].value.length < 10 || c[0].value == "" || c[0].value.length < 8 || b[0].value == "") {
             warning();
         } else {
-            window.open("./Hello.html")
+            window.location = "/Hello.html";
         }
     });
 });
