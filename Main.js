f32b749e1e87e576de690771890929a240722014
Aamir <aamir.zaidi2000@gmail.com>
2019-03-11 08:40:57 -0700

    Done

---
+++ Main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $("form").submit(function(e) {
+        e.preventDefault();
+    });
+    $("#Submit").on("click", (e) => {
+        let a = $("#Phone");
+        let b = $("#Email");
+        let c = $("#Pass");
+        let Exp = new RegExp( /[a-z,A-Z]/ , "gi");
+        let isNum = Exp.test(a[0].value.toString());
+        console.log(a[0].value.toString());
+        console.log(isNum);
+        if (a[0].value == "" || isNum == true || a[0].value.length < 10 || c[0].value == "" || c[0].value.length < 8 || b[0].value == "") {
+            warning();
+        } else {
+            window.open("./Hello.html")
+        }
+    });
+});
+
+
+function warning(){
+    $("#warning").addClass("Animate");
+            $("#warning").animate({
+                opacity: 1,
+                height: 50,
+                display:"block"
+            }, 1800)
+            setTimeout(() => {
+                $("#warning").removeClass("Animate");
+                $("#warning").animate({
+                    opacity: 0,
+                    height: 0,
+                    display:"none"
+                }, 0)
+             }, 8000);
+            $("#warning")[0].innerHTML = "Please Fill In All The Fields Correctly";
+}
